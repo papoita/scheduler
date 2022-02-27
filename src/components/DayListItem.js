50b0ee3c320e438602a50e23091967adeeb90908
PaolaP <paolaperez8@gmail.com>
2022-02-27 18:41:22 -0500

    props.name for days of the week

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function DayListItem(props) {
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
