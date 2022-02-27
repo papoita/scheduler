057d3026959d6a2da67d39484e906570b5e84ad8
PaolaP <paolaperez8@gmail.com>
2022-02-27 17:17:14 -0500

    base button and danger button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
-
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+
+   if (props.danger) {
+      buttonClass += " button--danger";
+    }
+ 
+   return <button className={buttonClass}>{props.children}</button>;
+ }
