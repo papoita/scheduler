2b970767e6c2e2d7010d787bdcf952a9a7649fd5
PaolaP <paolaperez8@gmail.com>
2022-02-27 18:23:39 -0500

    add DayListItem  hardcode values and stories

---
+++ src/components/DayListItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular">Day Name</h2> 
+      <h3 className="text--light">X spots remaining</h3>
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem"
 
 storiesOf("Button", module)
   .addParameters({
@@ -22,3 +22,15 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+storiesOf("DayListItem", module) 
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  }) 
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
+  ));
+  
