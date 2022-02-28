fbf514e5de184350cc0c245dde489f8c64fadfd8
PaolaP <paolaperez8@gmail.com>
2022-02-27 22:06:00 -0500

    useState as Monday

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   const days = [
     {
       id: 1,
@@ -33,8 +35,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={(day) => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
