6ab4c079c0a91d3bef67cd261e9d7caf8b33c902
PaolaP <paolaperez8@gmail.com>
2022-02-27 19:50:26 -0500

    styled days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,16 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("li", {
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full":props.spots===0
+
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
