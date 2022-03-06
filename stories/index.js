e97fccd513592bcffefee372c184e9cae3246140
PaolaP <paolaperez8@gmail.com>
2022-03-06 16:15:56 -0500

    Show component onclick delete and edit

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "../src/components/Appointment";
 import Header from "../src/components/Appointment/Header";
 import Empty from "../src/components/Appointment/Empty"
+import Show from "../src/components/Appointment/Show"
  
 
 storiesOf("Button", module)
@@ -145,3 +146,4 @@ storiesOf("DayListItem", module)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
 .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+.add("Show", ()=> <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
