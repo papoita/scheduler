7c3c2224f62463fdd43a2e1075a811a6ff8dba4c
PaolaP <paolaperez8@gmail.com>
2022-03-06 16:05:38 -0500

    Empty component

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
 import Appointment from "../src/components/Appointment";
 import Header from "../src/components/Appointment/Header";
+import Empty from "../src/components/Appointment/Empty"
  
 
 storiesOf("Button", module)
@@ -142,5 +143,5 @@ storiesOf("DayListItem", module)
         })
         .add("Appointment", () => <Appointment />)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
-        .add("Header", () => <Header time="12pm" />);
-
+        .add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty onAdd={action("onAdd")}/>)
