707ebb6a461907ca73c714b1de549fe8f70119e1
PaolaP <paolaperez8@gmail.com>
2022-03-06 19:55:43 -0500

    displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,20 @@
-
 import React from "react";
 import "./styles.scss"
-
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 
 export default function Appointment(props) {
   
   return(
+    
+      
     <article className="appointment">
-      {props.time? `Appointment at ${props.time}`: "No Appointments"}
+      <Header  time={props.time}/>
+      
+      {props.interview? <Show student={props.student} interviewer={props.interviewer}/>: <Empty /> }
       </article>
+    
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -169,4 +169,19 @@ storiesOf("Appointment", module)
   .add("Create", ()=>(
     <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />
   ))
-  
+.add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
