630a8f2b02b1f83ec09ced4c46b2bc0ff1bc0c97
PaolaP <paolaperez8@gmail.com>
2022-03-08 12:14:25 -0500

    import axios and useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios  from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
