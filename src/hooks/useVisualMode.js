4d5b1a6b9bf2ba6386dca618ec3530fbab97d0c3
PaolaP <paolaperez8@gmail.com>
2022-03-09 16:39:18 -0500

    custom hooks replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,13 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
     setMode(newMode);
-    setHistory([...history, newMode]);
+    if(replace){
+      setHistory([...history.slice(0, -1), newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
   }
 
   function back() {
