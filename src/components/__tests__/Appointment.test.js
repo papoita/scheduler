ece5f7893c471044ecdf6b83184607a26b63b396
PaolaP <paolaperez8@gmail.com>
2022-03-19 19:16:46 -0400

    test for appointment component using Jest

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("does something it is supposed to do", () => {
+    // ...
+  });
+
+  it("does something else it is supposed to do", () => {
+    // ...
+  });
+});
