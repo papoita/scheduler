1b5f318d7609f1ceeedb0d87dee1d90d0a67597d
PaolaP <paolaperez8@gmail.com>
2022-03-19 19:16:21 -0400

    testing dataype as array for interviewers

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const { onChange, interviewers, value } = props;
@@ -24,3 +25,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
