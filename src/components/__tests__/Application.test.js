1c6bbf9aaa7dbc8ef65592a00e0e6c9868007753
PaolaP <paolaperez8@gmail.com>
2022-03-21 20:16:33 -0400

    testing plan for application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,7 +6,18 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-describe("Appointment", () => {
+// # testing plan
+// Render the Application.
+// Wait until the text "Archie Cohen" is displayed.
+// Click the "Add" button on the first empty appointment.
+// Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+// Click the first interviewer in the list.
+// Click the "Save" button on that same appointment.
+// Check that the element with the text "Saving" is displayed.
+// Wait until the element with the text "Lydia Miller-Jones" is displayed.
+// Check that the DayListItem with the text "Monday" also has the text "no spots remaining"
+
+describe("Application", () => {
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
