822395494d4740a038adb02bec83ac67d917cf46
PaolaP <paolaperez8@gmail.com>
2022-03-26 17:36:25 -0400

    book an appointment cypress test before refactor

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,21 @@
+describe("Appointment", () => {
+  
+
+  it("should book an appointment", () => {
+    cy.request("GET", "/api/debug/reset")
+    cy.visit("/");
+    cy.contains("Monday");
+
+    cy.get("[alt=Add]")
+ .first()
+ .click();
+
+ cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+
+ cy.get("[alt='Sylvia Palmer']").click();
+ cy.contains("Save").click();
+
+ cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+});
