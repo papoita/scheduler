88aaaa84c486fc5fdceb63869791de1d94dd333e
PaolaP <paolaperez8@gmail.com>
2022-03-26 17:52:57 -0400

    delete interview cypress test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,37 +1,46 @@
 describe("Appointments", () => {
   beforeEach(() => {
-   cy.request("GET", "/api/debug/reset");
- 
-   cy.visit("/");
- 
-   cy.contains("Monday");
+    cy.request("GET", "/api/debug/reset");
+
+    cy.visit("/");
+
+    cy.contains("Monday");
   });
- 
+
   it("should book an interview", () => {
-   cy.get("[alt=Add]")
-    .first()
-    .click();
- 
-   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-   cy.get('[alt="Sylvia Palmer"]').click();
- 
-   cy.contains("Save").click();
- 
-   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-   cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.get("[alt=Add]").first().click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
   it("should edit an interview", () => {
-    cy.get("[alt=Edit]")
-     .first()
-     .click({ force: true });
+    cy.get("[alt=Edit]").first().click({ force: true });
 
-     cy.get("[data-testid=student-name-input]").clear().type("Paola");
-   cy.get('[alt="Tori Malcolm"]').click();
+    cy.get("[data-testid=student-name-input]").clear().type("Paola");
+    cy.get('[alt="Tori Malcolm"]').click();
 
-   cy.contains("Save").click();
+    cy.contains("Save").click();
 
-   cy.contains(".appointment__card--show", "Paola");
-   cy.contains(".appointment__card--show", "Tori Malcolm");
+    cy.contains(".appointment__card--show", "Paola");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
- });
+
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").first().click({ force: true });
+
+    cy.contains("Confirm").click();
+
+  cy.contains("Deleting").should("exist");
+  cy.contains("Deleting").should("not.exist");
+
+  cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+});
+});
