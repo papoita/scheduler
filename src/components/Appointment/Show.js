be3acf6106b4c9640a428458d30f11a40f246d30
PaolaP <paolaperez8@gmail.com>
2022-08-01 11:17:36 -0400

    add if interviewer is not passed avoid app crash

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -9,7 +9,7 @@ export default function Show (props){
     <h2 className="text--regular">{student}</h2>
     <section className="interviewer">
       <h4 className="text--light">Interviewer</h4>
-      <h3 className="text--regular">{interviewer.name}</h3>
+      <h3 className="text--regular">{interviewer? interviewer.name : interviewer}</h3>
     </section>
   </section>
   <section className="appointment__card-right">
